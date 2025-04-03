3d5fdca7e222cc533c553115744932320d0212cc
JuanDG05 <jdgarcias@udistrital.edu.co>
2025-04-03 13:42:41 -0500

    Add files via upload

---
+++ Corte1/ListasTuplasDiccionarios/diccionario1.py
@@ -0,0 +1,15 @@
+vocales =  {"Primera": "a", "Segunda": "e", "Tercera": "i", "Cuarta": "o", "Quinta": "u"}
+
+print("Las vocales son: ", vocales)
+
+cantidad_estudiantes = {'Programacion Aplicada': "64", "Programacion basica": "98", "Programacion Orientada a objetos": "87"}
+salones = {'Sabio Caldas 506', 'Sabio Caldas 503', 'Administrativo 402'}
+
+print("\n A continuacion, se mostraran las materias, su cantidad de estudiantes y posteriormente los salones en los que se dicta")
+print("\n", cantidad_estudiantes, salones)
+
+a = int(input("Ingrese la cantidad de usuarios para Programacion Aplicada: "))
+
+cantidad_estudiantes.update = ({'Programacion Aplicada': a})
+
+print(cantidad_estudiantes)
