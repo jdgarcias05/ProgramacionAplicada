47274c8a4fd9378cad27e895bb0470cf6704d69c
jdgarcias05 <jdgarcias@udistrital.edu.co>
2025-04-10 13:53:56 -0500

    Add files via upload

---
+++ Corte1/ListasTuplasDiccionarios/diccionario2.py
@@ -0,0 +1,5 @@
+carrerasUD = {"Ingenierías ": ["Electrónica","Industrial","Sistemas","Eléctrica", "Catastral y Geodesia"], "Licenciaturas" : ["Biología", "Educación Artística", "Educación Infantil"]}
+
+print(carrerasUD["Ingenierías "])
+print("\n ----------------------------------------\n ")
+print(carrerasUD["Licenciaturas"])
