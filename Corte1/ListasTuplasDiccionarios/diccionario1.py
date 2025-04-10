551f691726ff578cbe007e318d71ad955431df82
jdgarcias05 <jdgarcias@udistrital.edu.co>
2025-04-10 13:39:32 -0500

    Update diccionario1.py

--- Corte1/ListasTuplasDiccionarios/diccionario1.py
+++ Corte1/ListasTuplasDiccionarios/diccionario1.py
@@ -1,15 +1,29 @@
-vocales =  {"Primera": "a", "Segunda": "e", "Tercera": "i", "Cuarta": "o", "Quinta": "u"}
+#vocales =  {"Primera": "a", "Segunda": "e", "Tercera": "i", "Cuarta": "o", "Quinta": "u"}
 
-print("Las vocales son: ", vocales)
+#print("Las vocales son: ", vocales)
 
-cantidad_estudiantes = {'Programacion Aplicada': "64", "Programacion basica": "98", "Programacion Orientada a objetos": "87"}
-salones = {'Sabio Caldas 506', 'Sabio Caldas 503', 'Administrativo 402'}
+## Diccionario para gestión logística de cursos académicos.
+cantidad_estudiantes = {'Programacion Aplicada': "64",'Programacion Basica': "98",'Programacion Orientada a Objetos': "87"}
 
-print("\n A continuacion, se mostraran las materias, su cantidad de estudiantes y posteriormente los salones en los que se dicta")
-print("\n", cantidad_estudiantes, salones)
+salones = { 'Programacion Aplicada': 'Sabio Caldas 506','Programacion Basica': 'Sabio Caldas 503','Programacion Orientada a Objetos': 'Administrativo 402'}
 
-a = int(input("Ingrese la cantidad de usuarios para Programacion Aplicada: "))
-
-cantidad_estudiantes.update = ({'Programacion Aplicada': a})
-
+print("\n A continuación, se mostrarán las materias y su cantidad de estudiantes.")
+print(" ------------------------------- ")
 print(cantidad_estudiantes)
+
+print("\n A continuación, se mostrarán las materias y los salones donde se dicta.")
+print(" ------------------------------- ")
+print(salones)
+
+materianueva = input("\nIngrese el nombre para la nueva materia: ")
+cantidad_estudiantesmaterianueva = input("Ingrese la cantidad de usuarios para la nueva materia: ")
+salon = input("Ingrese el salón en el que se dicta la nueva materia: ")
+
+cantidad_estudiantes.update({materianueva: cantidad_estudiantesmaterianueva})
+salones.update({materianueva: salon})
+
+print("\n A continuación, se muestran las listas actualizadas:")
+print(cantidad_estudiantes)
+print(" ------------------------------- ")
+print(salones)
+
